79db97a4e7636627ad952d28920e3cbee110b6ef
José Iván Chapoñán Casiano <ivan@navike21.com>
2019-04-27 15:27:19 -0500

    implementado modal y cambios de texto

--- js/ready.js
+++ js/ready.js
@@ -2,17 +2,20 @@ $(document).ready(function () {
   $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     dots: false,
+    infinite: false,
     responsive: [{
-      breakpoint: 768,
+      breakpoint: 1025,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
-        dots: true
+        dots: true,
+        arrows: false,
+        infinite: false
       }
     }]
   });
@@ -22,3 +25,18 @@ $(document).ready(function () {
   });
   AOS.init();
 });
+
+showModal = (element) =>{
+  let img = element.getElementsByTagName('img')[0].src;
+  let caption = element.getElementsByTagName('img')[0].dataset.caption;
+  document.getElementById('imageModal').innerHTML = '<img src="'+img+'">';
+  document.getElementById('captionModal').innerHTML = '<p>'+caption+'</p>';
+
+  document.getElementsByClassName('wrappModal')[0].classList.add('showModal');
+  document.getElementsByClassName('overflow')[0].classList.add('showModalWrapp');
+}
+
+closeModal = () => {
+  document.getElementsByClassName('wrappModal')[0].classList.remove('showModal');
+  document.getElementsByClassName('overflow')[0].classList.remove('showModalWrapp');
+}
